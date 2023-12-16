331d7eb2a6e755f4f9617a9cdda6d3a774d6cc29
Andrii Bondarenko <just.bondarenko@gmail.com>
2023-12-16 16:42:28 +0200

    Initialize new project with TailwindCSS and DaisyUI

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
