25aa3ff790916fcbf1ccc1a4915a6626d3772c23
Andrii Bondarenko <just.bondarenko@gmail.com>
2023-12-16 17:19:19 +0200

    Instal font awesome components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import './index.css';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 
 app.mount('#app')
