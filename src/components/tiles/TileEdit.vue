eb0ef6be02a48617ceb5398c3a3abe05442d7806
Andrii Bondarenko <just.bondarenko@gmail.com>
2023-12-18 22:27:16 +0200

    Implement color picker for tile edit

--- src/components/tiles/TileEdit.vue
+++ src/components/tiles/TileEdit.vue
@@ -2,40 +2,38 @@
   <div class="flex flex-col gap-2 w-full">
     <div class="flex flex-col">
       <div class="flex flex-col w-100">
-      <div class="label">
-        <span class="label-text">URL</span>
+        <div class="label">
+          <span class="label-text">URL</span>
+        </div>
+        <input type="text" placeholder="https://" class="input input-bordered w-full" v-model="innerUrl" />
       </div>
-      <input type="text" placeholder="https://" class="input input-bordered w-full" v-model="innerUrl" />
-    </div>
-    <div class="flex flex-col w-100">
-      <div class="label">
-        <span class="label-text">Label / Page title</span>
+      <div class="flex flex-col w-100">
+        <div class="label">
+          <span class="label-text">Label / Page title</span>
+        </div>
+        <input type="text" placeholder="Page title" class="input input-bordered w-full" v-model="innerLabel"
+          :disabled="!innerUrl" />
       </div>
-      <input type="text" placeholder="Page title" class="input input-bordered w-full" v-model="innerLabel"
-        :disabled="!innerUrl" />
-    </div>
-    <div class="flex flex-col w-100">
-      <div class="label">
-        <span class="label-text">Background color</span>
+      <div class="color-pickers flex justify-between px-1 mt-2 w-100">
+        <div class="flex flex-row items-center w-1/3 h-12">
+          <span class="label-text">Background color</span>
+          <ColorPicker format="hex" :pure-color="{}" picker-type="fk" shape="circle" round-history disable-alpha lang="En"
+            v-model:pureColor="innerBgColor" />
+        </div>
+        <div class="flex flex-row items-center w-1/3 h-12">
+          <span class="label-text">Label color</span>
+          <ColorPicker format="hex" :pure-color="{}" picker-type="fk" shape="circle" round-history disable-alpha lang="En"
+            v-model:pureColor="innerFontColor" />
+        </div>
       </div>
-      <input type="text" placeholder="Any CSS color" class="input input-bordered w-full" v-model="innerBgColor"
-        :disabled="!innerUrl" />
-    </div>
-    <div class="flex flex-col w-100">
-      <div class="label">
-        <span class="label-text">Label color</span>
+      <div class="flex w-100">
+        <button class="btn btn-ghost mt-4 ml-0 mr-2" @click="$emit('close')">
+          <FontAwesomeIcon :icon="{ prefix: 'fas', iconName: 'xmark-circle' }" /> Close
+        </button>
+        <button class="btn btn-success mt-4 mr-0 ml-auto" @click="save" :disabled="!(!!innerUrl && !!innerLabel)">
+          <FontAwesomeIcon :icon="{ prefix: 'fas', iconName: 'floppy-disk' }" /> Save
+        </button>
       </div>
-      <input type="text" placeholder="Any CSS color" class="input input-bordered w-full"
-        v-model="innerFontColor" :disabled="!innerUrl" />
-    </div>
-    <div class="flex w-100">
-      <button class="btn btn-ghost mt-4 ml-0 mr-2" @click="$emit('close')">
-        <FontAwesomeIcon :icon="{ prefix: 'fas', iconName: 'xmark-circle' }" /> Close
-      </button>
-      <button class="btn btn-success mt-4 mr-0 ml-auto" @click="save" :disabled="!(!!innerUrl && !!innerLabel)">
-        <FontAwesomeIcon :icon="{ prefix: 'fas', iconName: 'floppy-disk' }" /> Save
-      </button>
-    </div>
     </div>
   </div>
 </template>
@@ -95,4 +93,9 @@ export default {
 }
 </script>
 
+<style lang="scss">
+.vc-color-wrap {
+  margin: auto auto; 
+}
+</style>
 
