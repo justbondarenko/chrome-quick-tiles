759876b76f2c58528be63193e225e8ad6d0aa602
Andrii Bondarenko <just.bondarenko@gmail.com>
2023-12-23 01:07:42 +0200

    Increase dropdown shadow

--- src/components/bookmarks/BookmarksDrawer.vue
+++ src/components/bookmarks/BookmarksDrawer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dropdown" :class="{'dropdown-top': settingsStore.toolbarPosition === 'bottom' }" >
     <div tabindex="0" role="button" class="btn flex-nowrap"><FontAwesomeIcon :icon="['far', 'bookmark']" /><template v-if="settingsStore.showBookmarksLabel">Bookmarks</template></div>
-    <BookmarksList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="flex-nowrap dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-96 mt-2 overflow-y-auto" />
+    <BookmarksList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="flex-nowrap dropdown-content z-[1] menu p-2 shadow-md bg-base-200 rounded-box w-96 mt-2 overflow-y-auto" />
   </div>
 </template>
 
--- src/components/recents/RecentsDrawer.vue
+++ src/components/recents/RecentsDrawer.vue
@@ -2,7 +2,7 @@
   <div class="dropdown" :class="{'dropdown-top': settingsStore.toolbarPosition === 'bottom' }" >
     <div tabindex="0" role="button" class="btn flex-nowrap"><FontAwesomeIcon :icon="['fas', 'clock-rotate-left']" class="fa-fw" /> <span
           v-if="settingsStore.showRecentlyClosedLabel">Recently closed</span></div>
-    <RecentsList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="bookmarks-list dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-96 mt-2 overflow-y-auto" />
+    <RecentsList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="bookmarks-list dropdown-content z-[1] menu p-2 shadow-md bg-base-100 rounded-box w-96 mt-2 overflow-y-auto" />
   </div>
 </template>
 
