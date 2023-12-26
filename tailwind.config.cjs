5e073f0995510d7563f45855aa74d42a7651f1b6
Andrii Bondarenko <just.bondarenko@gmail.com>
2023-12-26 10:40:11 +0200

    Add purgeCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require('@tailwindcss/typography'),],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography'),
+  ],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: 'dark', // name of one of the included themes for dark mode
