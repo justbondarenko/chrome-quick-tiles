00626425efe0fa15b847665692e4e4cb02f46ff9
Andrii Bondarenko <just.bondarenko@gmail.com>
2024-01-03 21:15:31 +0400

    Fix dropdowns visual

--- src/components/recents/RecentsDrawer.vue
+++ src/components/recents/RecentsDrawer.vue
@@ -2,7 +2,7 @@
   <div class="dropdown" :class="{'dropdown-top': settingsStore.toolbarPosition === 'bottom' }" >
     <div tabindex="0" role="button" class="btn flex-nowrap"><FontAwesomeIcon :icon="['fas', 'clock-rotate-left']" class="fa-fw" /> <span
           v-if="settingsStore.showRecentlyClosedLabel">Recently closed</span></div>
-    <RecentsList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="flex-nowrap dropdown-content z-[1] menu p-2 shadow-md bg-base-200 rounded-box w-96 mt-2 overflow-y-auto" />
+    <RecentsList tabindex="0" :class="{'mb-2': settingsStore.toolbarPosition === 'bottom', 'mt-2': settingsStore.toolbarPosition === 'top'}" class="flex-nowrap dropdown-content z-[2] menu p-2 shadow-md bg-base-200 rounded-box w-96 mt-2 overflow-y-auto" />
   </div>
 </template>
 
