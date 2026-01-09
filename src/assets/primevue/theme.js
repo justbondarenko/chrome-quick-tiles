906d0b5a756735045e458e3705ccf6782fd2fe96
Andrii Bondarenko <just.bondarenko@gmail.com>
2026-01-10 03:28:19 +0400

    Change base PrimeVue theme

--- src/assets/primevue/theme.js
+++ src/assets/primevue/theme.js
@@ -1,7 +1,7 @@
 import { definePreset } from '@primeuix/themes'
-import Aura from '@primeuix/themes/aura'
+import Lara from '@primeuix/themes/lara'
 
-const MyPreset = definePreset(Aura, {
+const MyPreset = definePreset(Lara, {
   semantic: {
     surface: {
       0: '#ffffff',
