da57e0b759bb9ca0f226e152cc4d97a990036218
Andrii Bondarenko <just.bondarenko@gmail.com>
2026-01-10 02:21:32 +0400

    Refactor TileAdd component to use PrimeVue components and script setup syntax

--- src/components/tiles/TileAdd.vue
+++ src/components/tiles/TileAdd.vue
@@ -1,24 +1,19 @@
 <template>
-  <div class="drawer drawer-end">
-    <input id="new-tile-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <label for="new-tile-drawer" class="btn drawer-button">
-        <i class="fa-solid fa-plus" />
-        <template v-if="settingsStore.showNewTileLabel">Add link</template>
-      </label>
-    </div>
-    <div class="drawer-side z-50">
-      <label
-        for="new-tile-drawer"
-        aria-label="close sidebar"
-        class="drawer-overlay"
-      ></label>
-      <div class="p-4 w-80 h-full bg-base-200 text-base-content flex flex-col">
-        <p class="prose-2xl">New Tile</p>
-        <div class="divider" />
-        <div
-          class="new-tile-wrapper w-full p-1 flex flex-col gap-4 h-full overflow-y-auto"
-        >
+  <div>
+    <Button
+      severity="secondary"
+      icon="fa-solid fa-plus"
+      :label="settingsStore.showNewTileLabel ? 'Add link' : undefined"
+      @click="visible = true"
+    />
+    <Drawer
+      v-model:visible="visible"
+      header="New Tile"
+      position="right"
+      class="!w-96"
+    >
+      <div class="flex flex-col gap-4 h-full">
+        <div class="new-tile-wrapper w-full flex flex-col gap-4 flex-1 overflow-y-auto">
           <TileElement
             :label="label"
             :url="url"
@@ -28,50 +23,42 @@
             class="pointer-events-none mx-auto"
           />
           <div class="flex flex-col gap-2">
-            <div class="flex flex-col w-100">
-              <div class="label">
-                <span class="label-text">URL</span>
-              </div>
-              <input
+            <div class="flex flex-col w-full">
+              <label for="tile-url-input" class="text-sm font-medium mb-1">URL</label>
+              <InputText
+                id="tile-url-input"
                 type="text"
                 placeholder="https://"
-                class="input input-bordered w-full max-w-xs"
+                class="w-full"
                 v-model="url"
                 @input="onUrlInput"
               />
             </div>
-            <div class="flex flex-col w-100">
-              <div class="label">
-                <span class="label-text">Label / Page title</span>
-              </div>
-              <input
+            <div class="flex flex-col w-full">
+              <label for="tile-label-input" class="text-sm font-medium mb-1">Label / Page title</label>
+              <InputText
+                id="tile-label-input"
                 type="text"
                 placeholder="Page title"
-                class="input input-bordered w-full max-w-xs"
+                class="w-full"
                 v-model="label"
                 :disabled="!url"
               />
             </div>
-            <div class="flex flex-col w-100">
-              <div class="label">
-                <span class="label-text">Size</span>
-              </div>
-              <div class="join w-fit">
-                <input
-                  v-for="option of getAvailableSizes()"
-                  :key="option"
-                  class="join-item btn btn-sm"
-                  type="radio"
-                  name="size-options"
-                  :aria-label="option.toUpperCase()"
-                  :checked="size === option"
-                  @click="size = option"
-                />
-              </div>
+            <div class="flex flex-col w-full">
+              <label for="tile-size-select" class="text-sm font-medium mb-1">Size</label>
+              <SelectButton
+                id="tile-size-select"
+                v-model="size"
+                :options="sizeOptions"
+                optionLabel="label"
+                optionValue="value"
+                aria-labelledby="tile-size-select"
+              />
             </div>
-            <div class="color-pickers flex flex-col justify-between px-1 mt-2 w-100">
+            <div class="color-pickers flex flex-col justify-between px-1 mt-2 w-full">
               <div class="flex flex-row items-center h-12 gap-3">
-                <span class="label-text">Background color</span>
+                <span class="text-sm font-medium">Background color</span>
                 <ColorPicker
                   format="hex"
                   :pure-color="{}"
@@ -84,7 +71,7 @@
                 />
               </div>
               <div class="flex flex-row items-center h-12 gap-3">
-                <span class="label-text">Label color</span>
+                <span class="text-sm font-medium">Label color</span>
                 <ColorPicker
                   format="hex"
                   :pure-color="{}"
@@ -99,128 +86,137 @@
             </div>
           </div>
         </div>
-        <button class="btn btn-success btn-outline mt-4" @click="save" :disabled="!url">
-          <i class="fa-solid fa-floppy-disk" /> Save
-        </button>
+        <div class="mt-auto pt-4 border-t border-surface-600">
+          <Button
+            severity="secondary"
+            icon="fa-solid fa-floppy-disk"
+            label="Save"
+            @click="save"
+            :disabled="!url"
+            class="w-full"
+          />
+        </div>
       </div>
-    </div>
+    </Drawer>
   </div>
 </template>
 
-<script>
-import { useSettingsStore } from "@/stores/settings";
-import TileElement from "./TileElement.vue";
+<script setup>
+import { ref } from 'vue'
+import { useSettingsStore } from '@/stores/settings'
+import TileElement from './TileElement.vue'
+import Button from 'primevue/button'
+import Drawer from 'primevue/drawer'
+import InputText from 'primevue/inputtext'
+import SelectButton from 'primevue/selectbutton'
 
-export default {
-  name: "TileAdd",
-  components: {
-    TileElement,
-  },
-  data() {
-    return {
-      settingsStore: useSettingsStore(),
-      label: "",
-      url: "",
-      fontColor: "white",
-      bgColor: "black",
-      size: "m",
-    };
-  },
-  methods: {
-    getAvailableSizes() {
-      return ["s", "m"];
-    },
-    onUrlInput() {
-      const url = new URL(chrome.runtime.getURL("/_favicon/"));
-      url.searchParams.set("pageUrl", this.url);
-      url.searchParams.set("size", "32");
-      console.log(url.toString());
-      this.getDominantColor(url.toString(), function (color) {
-        console.log(color);
-      });
-    },
-    save() {
-      const item = {
-        url: this.url,
-        label: this.label,
-        fontColor: this.fontColor,
-        bgColor: this.bgColor,
-        size: this.size,
-      };
-      this.$emit("saveTile", item);
-      document.getElementById("new-tile-drawer").checked = false;
-    },
-    getDominantColor(imageUrl, callback) {
-      const img = new Image();
-      img.crossOrigin = "Anonymous"; // Enable CORS for the image
-      img.src = imageUrl;
+const emit = defineEmits(['saveTile'])
 
-      img.onload = function () {
-        const blockSize = 5; // only visit every 5 pixels
-        const defaultRGB = { r: 0, g: 0, b: 0 }; // for non-supporting envs
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext && canvas.getContext("2d");
+const settingsStore = useSettingsStore()
+const visible = ref(false)
+const label = ref('')
+const url = ref('')
+const fontColor = ref('white')
+const bgColor = ref('black')
+const size = ref('m')
 
-        if (!context) {
-          callback(defaultRGB);
-          return;
-        }
+const sizeOptions = [
+  { label: 'S', value: 's' },
+  { label: 'M', value: 'm' }
+]
 
-        const height = (canvas.height =
-          img.naturalHeight || img.offsetHeight || img.height);
-        const width = (canvas.width = img.naturalWidth || img.offsetWidth || img.width);
-
-        context.drawImage(img, 0, 0);
-
-        try {
-          const imageData = context.getImageData(0, 0, width, height).data;
-          const colorMap = new Map();
-
-          for (let i = 0; i < imageData.length; i += blockSize * 4) {
-            const red = imageData[i];
-            const green = imageData[i + 1];
-            const blue = imageData[i + 2];
-
-            const color = `rgb(${red},${green},${blue})`;
-
-            if (colorMap.has(color)) {
-              colorMap.set(color, colorMap.get(color) + 1);
-            } else {
-              colorMap.set(color, 1);
-            }
-          }
-
-          let dominantColor = null;
-          let maxCount = 0;
-
-          colorMap.forEach((count, color) => {
-            if (count > maxCount) {
-              maxCount = count;
-              dominantColor = color;
-            }
-          });
-
-          callback(dominantColor);
-        } catch (e) {
-          /* security error, image on a different domain */
-          callback(defaultRGB);
-        }
-      };
-    },
-  },
-};
-</script>
-
-<style lang="scss">
-.page-settings-item {
-  display: flex;
-  flex-direction: column;
-  border-radius: 15px;
-  padding: 15px;
+const onUrlInput = () => {
+  // eslint-disable-next-line
+  const faviconUrl = new URL(chrome.runtime.getURL('/_favicon/'))
+  faviconUrl.searchParams.set('pageUrl', url.value)
+  faviconUrl.searchParams.set('size', '32')
+  console.log(faviconUrl.toString())
+  getDominantColor(faviconUrl.toString(), (color) => {
+    console.log(color)
+  })
 }
 
-.drawer-side {
-  max-width: 100vw;
-  overflow-x: hidden;
+const save = () => {
+  const item = {
+    url: url.value,
+    label: label.value,
+    fontColor: fontColor.value,
+    bgColor: bgColor.value,
+    size: size.value
+  }
+  emit('saveTile', item)
+  visible.value = false
+  resetForm()
+}
+
+const resetForm = () => {
+  label.value = ''
+  url.value = ''
+  fontColor.value = 'white'
+  bgColor.value = 'black'
+  size.value = 'm'
+}
+
+const getDominantColor = (imageUrl, callback) => {
+  const img = new Image()
+  img.crossOrigin = 'Anonymous'
+  img.src = imageUrl
+
+  img.onload = function () {
+    const blockSize = 5
+    const defaultRGB = { r: 0, g: 0, b: 0 }
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext && canvas.getContext('2d')
+
+    if (!context) {
+      callback(defaultRGB)
+      return
+    }
+
+    const height = (canvas.height =
+      img.naturalHeight || img.offsetHeight || img.height)
+    const width = (canvas.width = img.naturalWidth || img.offsetWidth || img.width)
+
+    context.drawImage(img, 0, 0)
+
+    try {
+      const imageData = context.getImageData(0, 0, width, height).data
+      const colorMap = new Map()
+
+      for (let i = 0; i < imageData.length; i += blockSize * 4) {
+        const red = imageData[i]
+        const green = imageData[i + 1]
+        const blue = imageData[i + 2]
+
+        const color = `rgb(${red},${green},${blue})`
+
+        if (colorMap.has(color)) {
+          colorMap.set(color, colorMap.get(color) + 1)
+        } else {
+          colorMap.set(color, 1)
+        }
+      }
+
+      let dominantColor = null
+      let maxCount = 0
+
+      colorMap.forEach((count, color) => {
+        if (count > maxCount) {
+          maxCount = count
+          dominantColor = color
+        }
+      })
+
+      callback(dominantColor)
+    } catch (e) {
+      callback(defaultRGB)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.new-tile-wrapper {
+  min-height: 0;
 }
 </style>
